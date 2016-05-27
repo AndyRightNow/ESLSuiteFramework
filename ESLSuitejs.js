7c285da65ad6a9b87bafef47d62afa96e3951484
AndyRightNow <naruto950726@hotmail.com>
2016-05-27 12:36:57 +0800

    Update Scroll to internal link

--- ESLSuitejs.js
+++ ESLSuitejs.js
@@ -1,14 +1,14 @@
 /* jshint esversion: 6 */
-(function() {
+
+$(document).ready(() => {
     "use strict";
     //----------------------------
-    //	Utility
+    //  Utility namespace
     //----------------------------
     var Utility = {
-
-        //---------------------------
-        //	CSS Class style handler
-        //---------------------------
+        //---------------------------------------
+        //  Add CSS Class object to the DOM
+        //---------------------------------------
         addCSS: function(jqueryObject, classObject) {
             if (typeof jqueryObject !== "undefined" && typeof classObject !== "undefined") {
                 for (let prop in classObject) {
@@ -16,6 +16,10 @@
                 }
             }
         },
+
+        //-------------------------------
+        //  Store and restore inline CSS
+        //-------------------------------
         storeInlineCSS: function(jqueryObject) {
             if (typeof jqueryObject !== "undefined") {
                 let styleStr = jqueryObject.attr('style');
@@ -30,12 +34,18 @@
     };
 
     //-----------------------------------
-    //	Scroll to show with "scrollshow"
+    //
+    //  Scroll to show 
+    //
+    //  Usage: Add CSS Class "scrollshow"
+    //  (case sensitive) to the HTML element
+    //  you want to animate.
+    //
     //-----------------------------------
     (function() {
-        $(window).on("scroll", function(event) {
-            var transitionTime = 600;	//Transition time in millisecond
-            var transTimeInSec = transitionTime / 1000;		//Transition time in second
+        $(window).scroll(function(event) {
+            var transitionTime = 600; //Transition time in millisecond
+            var transTimeInSec = transitionTime / 1000; //Transition time in second
 
             var transition = { //CSS Class object for showing element
                 "transition": "opacity " + transTimeInSec + "s ease-in-out",
@@ -48,16 +58,16 @@
             var currElem = elements.shift();
 
             if (currElem !== undefined) {
-                var windowBottom = $(window).scrollTop() + $(window).height(),
+                let windowBottom = $(window).scrollTop() + $(window).height(),
                     curEleBottom = $(currElem).offset().top + $(currElem).height();
 
                 if (windowBottom >= curEleBottom) {
-                    let styleStr = Utility.storeInlineCSS($(currElem));// Store the inline css style
+                    let styleStr = Utility.storeInlineCSS($(currElem)); // Store the inline css style
 
                     Utility.addCSS($(currElem), transition);
                     $(currElem).removeClass('scrollshow');
 
-                    setTimeout(function() {  //After the animation, restore the inline style
+                    setTimeout(function() { //After the animation, restore the inline style
                         Utility.restoreInlineCSS($(currElem), styleStr);
                     }, transitionTime - 1);
                 }
@@ -65,5 +75,31 @@
         });
     })();
 
-})();
+    //-------------------------------------
+    //
+    //  Scroll to internal link
+    //
+    //  Usage: Add CSS Class "scrolllink"
+    //  (case sensitive) to the HTML element
+    //  you want to animate.
+    //
+    //-----------------------------------
+    (function() {
+        $(".scrolllink").on('click', function(event) {
+            var thisHref = $(event.target).attr('href');
+            if (typeof thisHref !== "undefined") { //  Check if it's on an anchor tag or with href
+                if (thisHref[0] === '#') { //  Check if it's internal link
+                    event.preventDefault();
+                    let thisHash = this.hash;
+                    let animateTime = 600;
+                    $('html, body').animate({
+                        scrollTop: $(thisHash).offset().top
+                    }, animateTime, () => {
+                        window.location.hash = thisHash;
+                    });
+                }
+            }
+        });
+    })();
+});
 
